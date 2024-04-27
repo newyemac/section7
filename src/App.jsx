a36079abbc13393b505583865f721aeb9494ba9a
ryusuik <newyemac@gmail.com>
2024-04-27 15:57:11 +0900

    last update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import './App.css'
 import Viewer from './components/Viewer'
 import Controller from './components/Controller'
-import { useState } from 'react'
+import Even from './components/Even'
+import { useState, useEffect, useRef } from 'react'
 
 function App() {
   const [count,setCount] = useState(0)
+  const isMount = useRef(false) // 초기값 설정
 
+        //1. 마운트 : 탄생
+        useEffect(()=>{
+          console.log("mount")
+        },[])
+
+        //2. 업데이트 : 변화, 리렌더링
+        useEffect(()=>{
+          if(!isMount.current){
+            isMount.current = true
+            return
+          }
+          console.log("update")
+        })
+        //3. 언마운트 : 죽음
   const onClickButton = (value) =>{
     setCount(count + value)
   }
@@ -16,6 +32,7 @@ function App() {
     <h1>Simple counter</h1>
     <section>
       <Viewer count={count}/>
+      {count % 2 === 0 ? <Even /> : null}
     </section>
     <section>
       <Controller onClickButton={onClickButton}/>
